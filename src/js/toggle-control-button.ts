a1b0c28bde8a0591a56826574b64d2ad2fb31291
Rodrigo Passos <rodrigopassos@mac.com>
2022-06-16 22:12:36 -0400

    add click outside handler

--- src/js/toggle-control-button.ts
+++ src/js/toggle-control-button.ts
@@ -15,10 +15,9 @@
 
 const TOGGLE_CLOSE_EVENT = 'close'
 const TOGGLE_OPEN_EVENT = 'open'
+const TARGET_ACTIVE_CLASS = 'active'
 
 const ToggleButton = (( el: Element) => {
-
-  const isActive = (el: HTMLElement) => el.ariaExpanded === 'true'
 
   const getToggleTarget = (button: HTMLButtonElement): string => {
     return button.dataset.toggleControl || ''
@@ -28,6 +27,39 @@ const ToggleButton = (( el: Element) => {
     const targetEl = document.getElementById(selector)
     if (targetEl === null) throw new Error('[ToggleButton] The target element does not exist')
     return targetEl
+  }
+
+  const registerOutsiteClickHandler = (
+    button: HTMLButtonElement,
+  ) => {
+
+    const targetEl = getTargetEl(getToggleTarget(button))
+
+    if (targetEl) {
+
+      document.body.onclick = null
+
+      const closeIfClickOutside = (e: Event) => {
+        if (e.target instanceof Element) {
+
+          const eventTarget = e.target as Element
+
+          const isClosest = eventTarget.closest(`#${targetEl.id}`)
+
+          if (
+            !isClosest
+            && targetEl.classList.contains(TARGET_ACTIVE_CLASS)
+          ) {
+            button.dispatchEvent(new Event(TOGGLE_CLOSE_EVENT))
+            document.body.onclick = null
+          }
+        }
+      }
+
+      setTimeout(() => {
+        document.body.onclick = closeIfClickOutside
+      }, 100)
+    }
   }
 
   const updateNavButtons = (buttons: Element[], callback: (button: Element) => Element) => {
@@ -40,6 +72,8 @@ const ToggleButton = (( el: Element) => {
     updateNavButtons(Array.from(buttons), (button) => {
       if (button !== thisButton) {
         button.dispatchEvent(new Event(TOGGLE_CLOSE_EVENT))
+      } else {
+        registerOutsiteClickHandler(button as HTMLButtonElement)
       }
       return button
     })
@@ -51,9 +85,9 @@ const ToggleButton = (( el: Element) => {
     const targetEl = getTargetEl(getToggleTarget(button))
     if (btnState) {
       closeGrouppedTargets(button)
-      return targetEl?.classList.add('active')
+      return targetEl?.classList.add(TARGET_ACTIVE_CLASS)
     }
-    return targetEl?.classList.remove('active')
+    return targetEl?.classList.remove(TARGET_ACTIVE_CLASS)
   }
 
   /**
