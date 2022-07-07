5f211e545d5f3c0b3fcb0aa86bc710e52b7c1997
Rodrigo Passos <rodrigopassos@mac.com>
2022-07-07 19:42:51 -0400

    increase headroom tolerance
    https://github.com/dative/eosi-frontend-mix/issues/34

--- src/js/app.ts
+++ src/js/app.ts
@@ -8,6 +8,7 @@ const siteHeaderEl = document.getElementById("site-header");
 
 const siteHeader = new Headroom(siteHeaderEl, {
   offset: 144,
+  tolerance: { up: 20, down: 10 },
   onUnpin: () => {
     const hamburger = document.getElementById("hamburger-menu-button");
     hamburger?.dispatchEvent(new Event(TOGGLE_CLOSE_EVENT));
